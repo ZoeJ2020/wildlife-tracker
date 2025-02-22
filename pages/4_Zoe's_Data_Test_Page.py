906c477d53c8a097d309097c84abf022d52d73a3
Zoe Johnston <82781352+ZoeJ2020@users.noreply.github.com>
2025-02-22 22:02:26 +0000

    added data page for sqlite functionality

---
+++ pages/4_Zoe's_Data_Test_Page.py
@@ -0,0 +1,13 @@
+
+import streamlit as st
+from st_files_connection import FilesConnection
+
+st.set_page_config(page_title="Zoe's Data Test")
+
+st.markdown("# Zoe's Data Test")
+st.sidebar.header("Zoe's Data Test")
+st.write(
+    """test for data retriieval from database"""
+)
+
+# CODE BELOW!!!
