94948cc4ef0db6fc44607cc53f65ba1f91c6edef
kayla <146110376+kaylamasson@users.noreply.github.com>
2025-02-23 09:51:02 +0000

    editing captions

--- pages/3_Explore_Wildlife.py
+++ pages/3_Explore_Wildlife.py
@@ -59,8 +59,7 @@ if submit_button:
         if rows:
             for row in rows:
                 st.image(row[1])
-                st.header(row[4])
-                st.subheader(row[2])
+                st.subheader(row[4] + " " + row[2])
         else:
             st.write("No results found.")
     else:
@@ -74,8 +73,8 @@ else:
 
     for row in rows:
         st.image(row[1])
-        st.header(row[4])
-        st.subheader(row[2])
+        st.subheader(row[4] + " " + row[2])
+        #st.subheader(row[2])
 
 conn.close()
 
